e12062ec33e87ec888a926a433ca434573138208
mjasprec <michaeljosephasprec1@gmail.com>
2023-05-13 19:54:19 +0800

    feat: routing and logic

--- server.js
+++ server.js
@@ -11,14 +11,80 @@ const myEmitter = new Emitter();
 
 const PORT = process.env.PORT || 3500;
 
+const serveFile = async (filePath, contentType, response) => {
+  try {
+    const data = await fsPromises.readFile(filePath, 'utf8');
+    response.writeHead(200, { 'Content-type': contentType });
+    response.end(data);
+  } catch (error) {
+    console.log(error);
+    response.statusCode = 500;
+    response.end();
+  }
+};
+
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
+
+  const extension = path.extname(req.url);
+
+  let contentType;
+
+  switch (extension) {
+    case '..css':
+      contentType = 'text/css';
+      break;
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+    case '.jpg':
+      contentType = 'image/jpeg';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    case '.txt':
+      contentType = 'text/plain';
+      break;
+    default:
+      contentType = 'text/html';
+  }
+
+  let filePath =
+    contentType === 'text/html' && req.url === '/'
+      ? path.join(__dirname, 'views', 'index.html')
+      : contentType === 'text/html' && req.url.slice(-1) === '/'
+      ? path.join(__dirname, 'views', req.url, 'index.html')
+      : contentType === 'text/html'
+      ? path.join(__dirname, 'views', req.url)
+      : path.join(__dirname, req.url);
+
+  // fallback if extension is no provided
+  if (!extension && req.url.slice(-1) !== '/') {
+    filePath += '.html';
+  }
+
+  const fileExists = fs.existsSync(filePath);
+
+  if (fileExists) {
+    serveFile(filePath, contentType, res);
+  } else {
+    switch (path.parse(filePath).base) {
+      case 'old-page.html':
+        res.writeHead(301, { Location: '/new-page.html' });
+        res.end();
+        break;
+      case 'www.page.html':
+        res.writeHead(301, { Location: '/' });
+        res.end();
+        break;
+      default:
+        serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res);
+    }
+  }
 });
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-
-// myEmitter.on('log', (msg) => logEvents(msg));
-
-// setTimeout(() => {
-//   myEmitter.emit('log', 'Ayyayaya');
-// }, 2000);
