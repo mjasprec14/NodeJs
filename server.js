52328fc32ec693d32c842e6fca73c3a89a0521c6
mjasprec <michaeljosephasprec1@gmail.com>
2023-05-14 21:18:56 +0800

    feat: basic server

--- server.js
+++ server.js
@@ -1,23 +1,38 @@
+const logEvents = require('./logEvents');
+const EventEmitter = require('events');
+class Emitter extends EventEmitter {}
+
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
 const fsPromises = require('fs').promises;
 
-const logEvents = require('./logEvents');
-const EventEmitter = require('events');
-class Emitter extends EventEmitter {}
-
 const myEmitter = new Emitter();
+
+myEmitter.on('log', (msg, fileName) => logEvents(msg, fileName));
 
 const PORT = process.env.PORT || 3500;
 
 const serveFile = async (filePath, contentType, response) => {
   try {
-    const data = await fsPromises.readFile(filePath, 'utf8');
-    response.writeHead(200, { 'Content-type': contentType });
-    response.end(data);
+    const rawData = await fsPromises.readFile(
+      filePath,
+      !contentType.includes('image') ? 'utf8' : ''
+    );
+
+    const data =
+      contentType === 'application/json' ? JSON.parse(rawData) : rawData;
+
+    response.writeHead(filePath.includes('404.html') ? 404 : 200, {
+      'Content-Type': contentType,
+    });
+
+    response.end(
+      contentType === 'application/json' ? JSON.stringify(data) : data
+    );
   } catch (error) {
     console.log(error);
+    myEmitter.emit('log', `${error.name}: ${error.message}`, 'errLog.txt');
     response.statusCode = 500;
     response.end();
   }
@@ -25,13 +40,14 @@ const serveFile = async (filePath, contentType, response) => {
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
+  myEmitter.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt');
 
   const extension = path.extname(req.url);
 
   let contentType;
 
   switch (extension) {
-    case '..css':
+    case '.css':
       contentType = 'text/css';
       break;
     case '.js':
@@ -41,7 +57,7 @@ const server = http.createServer((req, res) => {
       contentType = 'application/json';
       break;
     case '.jpg':
-      contentType = 'image/jpeg';
+      contentType = 'image/jpg';
       break;
     case '.png':
       contentType = 'image/png';
@@ -62,7 +78,6 @@ const server = http.createServer((req, res) => {
       ? path.join(__dirname, 'views', req.url)
       : path.join(__dirname, req.url);
 
-  // fallback if extension is no provided
   if (!extension && req.url.slice(-1) !== '/') {
     filePath += '.html';
   }
@@ -70,21 +85,24 @@ const server = http.createServer((req, res) => {
   const fileExists = fs.existsSync(filePath);
 
   if (fileExists) {
+    // serve file
     serveFile(filePath, contentType, res);
   } else {
+    // 404 or 301 redirect
     switch (path.parse(filePath).base) {
       case 'old-page.html':
         res.writeHead(301, { Location: '/new-page.html' });
         res.end();
         break;
-      case 'www.page.html':
+      case 'www-page.html':
         res.writeHead(301, { Location: '/' });
         res.end();
         break;
       default:
+        // serve a 404 response
         serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res);
     }
   }
 });
 
-server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server listening on port:${PORT}`));
