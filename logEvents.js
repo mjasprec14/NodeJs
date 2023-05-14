9a7a36572f0087c14bf4620a15124a7822d7441a
mjasprec <michaeljosephasprec1@gmail.com>
2023-05-14 21:19:07 +0800

    feat: log event

--- logEvents.js
+++ logEvents.js
@@ -5,20 +5,17 @@ const path = require('path');
 const fs = require('fs');
 const fsPromises = require('fs').promises;
 
-const logEvents = async (message) => {
+const logEvents = async (message, logName) => {
   const id = uuid();
   const dateTime = format(new Date(), 'yyyy-MM-dd\tHH:mm:ss');
-  const logItem = `${dateTime}\t${id}\t${message}`;
+  const logItem = `\t${dateTime}\t${id}\t${message}`;
 
   try {
     if (!fs.existsSync(path.join(__dirname, 'logs'))) {
       await fsPromises.mkdir(path.join(__dirname, 'logs'));
     }
 
-    await fsPromises.appendFile(
-      path.join(__dirname, 'logs', 'eventLog.txt'),
-      logItem
-    );
+    await fsPromises.appendFile(path.join(__dirname, 'logs', logName), logItem);
   } catch (error) {
     console.log(error);
   }
