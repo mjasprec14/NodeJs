2a8edd87aa48ac8a54f979c5f75ca6269838a0d4
mjasprec <michaeljosephasprec1@gmail.com>
2023-05-19 21:43:02 +0800

    feat: Employees Controller

--- server.js
+++ server.js
@@ -40,9 +40,9 @@ app.use(express.json());
 
 // serve static files
 app.use('/', express.static(path.join(__dirname, '/public')));
-
 app.use('/subdir', express.static(path.join(__dirname, '/public')));
 
+// routes
 app.use('/', require('./routes/root'));
 app.use('/subdir', require('./routes/subdir'));
 app.use('/employees', require('./routes/api/employees'));
